b1e4ea4dc5285e301b0cb70d1b033bec82c7cb7e
Eigil Obrestad <eigil-github@obrestad.org>
2016-01-29 17:46:40 +0100

    Postfix configuration

--- manifests/mailserver.pp
+++ manifests/mailserver.pp
@@ -1,55 +1,121 @@
 class profile::mailserver {
+  $mysql_name = hiera("profile::mail::db::name")
+  $mysql_host = hiera("profile::mail::db::hose")
+  $mysql_user = hiera("profile::mail::db::user")
+  $mysql_pass = hiera("profile::mail::db::pass")
+
+  $mail_mynetworks = hiera("profile::mail::mynetworks")
+
+  $virtual_domains = @(END)
+    user = ${mysql_user}
+    password = ${mysql_pass}
+    hosts = ${mysql_host}
+    dbname = ${mysql_name}
+    query = SELECT 1 FROM virtual_domains WHERE name='%s'
+  END
+
+  $virtual_mailbox = @(END)
+    user = ${mysql_user}
+    password = ${mysql_pass}
+    hosts = ${mysql_host}
+    dbname = ${mysql_name}
+    query = SELECT 1 FROM virtual_users WHERE email='%s'
+  END
+
+  $virtual_alias = @(END)
+    user = ${mysql_user}
+    password = ${mysql_pass}
+    hosts = ${mysql_host}
+    dbname = ${mysql_name}
+    query = SELECT destination FROM virtual_aliases WHERE source='%s'
+  END
+
+  $virtual_email2email = @(END)
+    user = ${mysql_user}
+    password = ${mysql_pass}
+    hosts = ${mysql_host}
+    dbname = ${mysql_name}
+    query = SELECT email FROM virtual_users WHERE email='%s'
+  END
+
+  file { "/var/tmp/maildb-schema.sql":
+    owner => "root",
+    group => "root",
+    mode  => 744,
+    source => "puppet:///modules/profile/initial/maildb.sql",
+  }->
+  mysql::db { $mysql_name:
+    user     => $mysql_user,
+    password => $mysql_pass,
+    host     => $mysql_host,
+    grant    => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+    sql      => '/var/tmp/maildb-schema.sql',
+    import_timeout => 900,
+	require  => Class['::mysql::server'],
+  }
+
   firewall { '010 accept incoming SMTP':
     proto  => 'tcp',
 	dport  => 25,
     action => 'accept',
   }
+
   class { '::postfix::server':
     myhostname              => "$::fqdn",
     mydomain                => "$::domain",
-    mydestination           => "\$myhostname, localhost.\$mydomain, localhost, $fqdn",
-    inet_interfaces         => 'all',
-    message_size_limit      => '15360000', # 15MB
     mail_name               => 'Rothaugane mail',
+    myorigin              => "/etc/mailname",
+    mydestination       => "localhost",
+	mynetworks			=> $mail_mynetworks,
+
+    ssl                   => 'mail.rothaugane.com',
+    submission            => true,
+	smtpd_tls_cert_file => '/etc/dovecot/dovecot.pem',
+    smtpd_tls_key_file => '/etc/dovecot/private/dovecot.pem',
+
+    extra_main_parameters => {
+      'smtpd_tls_auth_only' => 'yes',
+      'mailbox_size_limit' => '0',
+	  'smtpd_relay_restrictions' => [
+        'permit_mynetworks',
+        'permit_sasl_authenticated',
+        'reject_unauth_destination',
+      ],
+    },
+    message_size_limit      => '15360000', # 15MB
+    smtpd_sasl_auth       => true,
+
     virtual_mailbox_domains => [
-      'proxy:mysql:/etc/postfix/mysql_virtual_domains_maps.cf',
+      'mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf',
     ],
     virtual_alias_maps      => [
-      'proxy:mysql:/etc/postfix/mysql_virtual_alias_maps.cf',
-      'proxy:mysql:/etc/postfix/mysql_virtual_alias_domain_maps.cf',
-      'proxy:mysql:/etc/postfix/mysql_virtual_alias_domain_catchall_maps.cf',
+      'mysql:/etc/postfix/mysql-virtual-alias-maps.cf',
+      'mysql:/etc/postfix/mysql-virtual-email2email.cf',
     ],
-    virtual_transport         => 'dovecot',
-    # if you want dovecot to deliver user+foo@example.org to user@example.org,
-    # uncomment this: (c.f. http://wiki2.dovecot.org/LDA/Postfix#Virtual_users)
-    # dovecot_destination     => '${user}@${nexthop}',
-    smtpd_sender_restrictions => [
-      'permit_mynetworks',
-      'reject_unknown_sender_domain',
+	virtual_mailbox_maps => [
+      'mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf',
     ],
+    virtual_transport         => 'lmtp:unix:private/dovecot-lmtp',
+
     smtpd_recipient_restrictions => [
       'permit_sasl_authenticated',
       'permit_mynetworks',
       'reject_unauth_destination',
+      'reject_rbl_client zen.spamhaus.org',
+      'reject_rhsbl_reverse_client dbl.spamhaus.org',
+      'reject_rhsbl_helo dbl.spamhaus.org',
+      'reject_rhsbl_sender dbl.spamhaus.org',
     ],
-    smtpd_sasl_auth       => true,
-    sender_canonical_maps => 'regexp:/etc/postfix/sender_canonical',
-    ssl                   => 'mail.rothaugane.com',
-    submission            => true,
-    header_checks         => [
-      '# Remove LAN (Webmail) headers',
-      '/^Received: from .*\.example\.ici/ IGNORE',
-      '# Sh*tlist',
-      '/^From: .*@(example\.com|example\.net)/ REJECT Spam, go away',
-      '/^From: .*@(lcfnl\.com|.*\.cson4\.com|.*\.idep4\.com|.*\.gagc4\.com)/  REJECT user unknown',
+
+    smtpd_sender_restrictions => [
+      'permit_mynetworks',
+      'reject_unknown_sender_domain',
     ],
-    postgrey              => true,
-    spamassassin          => true,
-    sa_skip_rbl_checks    => '0',
-    spampd_children       => '4',
-    # Send all emails to spampd on 10026
-    smtp_content_filter   => [ 'smtp:127.0.0.1:10026', ] ,
-    # This is where we get emails back from spampd
-    master_services       => [ '127.0.0.1:10027 inet n - n -20 smtpd'],
+	
+	recipient_delimiter => '-',
+    inet_interfaces         => 'all',
+
+    postgrey              => false,
+    spamassassin          => false,
   }
 }
