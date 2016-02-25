635e436ccca8e13c14af72e0dcb43e77a5fc61b3
Eigil Obrestad <eigil-github@obrestad.org>
2016-02-25 20:58:46 +0100

    Added service-control

--- manifests/dlnaserver.pp
+++ manifests/dlnaserver.pp
@@ -2,41 +2,51 @@ class profile::dlnaserver {
 	package { 'minidlna':
 		ensure => present,
 	}
+	
+	service { 'minidlna':
+		ensure => running,
+	}
 
 	ini_setting { "Media folder":
 		ensure  => present,
 		path    => '/etc/minidlna.conf',
 		setting => 'media_dir',
 		value   => '/srv/media',
+		notify => Service['minidlna'],
 	}	
 	ini_setting { "DbDir":
 		ensure  => present,
 		path    => '/etc/minidlna.conf',
 		setting => 'db_dir',
 		value   => '/var/cache/minidlna',
+		notify => Service['minidlna'],
 	}	
 	ini_setting { "LogDir":
 		ensure  => present,
 		path    => '/etc/minidlna.conf',
 		setting => 'log_dir',
 		value   => '/var/log',
+		notify => Service['minidlna'],
 	}	
 	ini_setting { "NetIF":
 		ensure  => present,
 		path    => '/etc/minidlna.conf',
 		setting => 'network_interface',
 		value   => 'eth0',
+		notify => Service['minidlna'],
 	}	
 	ini_setting { "friendly_name":
 		ensure  => present,
 		path    => '/etc/minidlna.conf',
 		setting => 'friendly_name',
 		value   => 'Mediaserveren',
+		notify => Service['minidlna'],
 	}	
 	ini_setting { "inotify":
 		ensure  => present,
 		path    => '/etc/minidlna.conf',
 		setting => 'inotify',
 		value   => 'yes',
+		notify => Service['minidlna'],
 	}	
 }
