44a3685b73301413b285a512cf7d17806377ef6e
Eigil Obrestad <eigil-github@obrestad.org>
2016-02-25 20:14:02 +0100

    Setting correct media-directory

--- manifests/dlnaserver.pp
+++ manifests/dlnaserver.pp
@@ -2,5 +2,11 @@ class profile::dlnaserver {
 	package { 'minidlna':
 		ensure => present,
 	}
-  
+
+	ini_setting { "Media folder":
+		ensure  => present,
+		path    => '/etc/minidlna.conf',
+		setting => 'media_dir',
+		value   => '/srv/media',
+	}	
 }
