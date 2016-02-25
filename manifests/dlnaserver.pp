beb73c05269e220ad0269efe8c343359b50709bf
Eigil Obrestad <eigil-github@obrestad.org>
2016-02-25 20:54:45 +0100

    Added more dlna settings

--- manifests/dlnaserver.pp
+++ manifests/dlnaserver.pp
@@ -9,4 +9,34 @@ class profile::dlnaserver {
 		setting => 'media_dir',
 		value   => '/srv/media',
 	}	
+	ini_setting { "DbDir":
+		ensure  => present,
+		path    => '/etc/minidlna.conf',
+		setting => 'db_dir',
+		value   => '/var/cache/minidlna',
+	}	
+	ini_setting { "LogDir":
+		ensure  => present,
+		path    => '/etc/minidlna.conf',
+		setting => 'log_dir',
+		value   => '/var/log',
+	}	
+	ini_setting { "NetIF":
+		ensure  => present,
+		path    => '/etc/minidlna.conf',
+		setting => 'network_interface',
+		value   => 'eth0',
+	}	
+	ini_setting { "friendly_name":
+		ensure  => present,
+		path    => '/etc/minidlna.conf',
+		setting => 'friendly_name',
+		value   => 'Mediaserveren',
+	}	
+	ini_setting { "inotify":
+		ensure  => present,
+		path    => '/etc/minidlna.conf',
+		setting => 'inotify',
+		value   => 'yes',
+	}	
 }
