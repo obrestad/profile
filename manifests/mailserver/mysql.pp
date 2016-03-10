0fc559abc278065f6952ec79f8e12713023838be
Eigil Obrestad <eigil-github@obrestad.org>
2016-03-10 21:15:04 +0100

    Syntax etc.

--- manifests/mailserver/mysql.pp
+++ manifests/mailserver/mysql.pp
@@ -1,8 +1,8 @@
 class profile::mailserver::mysql {
-  $mysql_name = hiera("profile::mail::db::name")
-  $mysql_host = hiera("profile::mail::db::host")
-  $mysql_user = hiera("profile::mail::db::user")
-  $mysql_pass = hiera("profile::mail::db::pass")
+  $mysql_name = hiera('profile::mail::db::name')
+  $mysql_host = hiera('profile::mail::db::host')
+  $mysql_user = hiera('profile::mail::db::user')
+  $mysql_pass = hiera('profile::mail::db::pass')
 
   $virtual_domains = "user = ${mysql_user}
 password = ${mysql_pass}
@@ -28,52 +28,52 @@ hosts = ${mysql_host}
 dbname = ${mysql_name}
 query = SELECT email FROM virtual_users WHERE email='%s'"
 
-  file { "/etc/postfix/mysql-virtual-mailbox-domains.cf":
-    owner   => "root",
-    group   => "root",
-	ensure  => "file",
+  file { '/etc/postfix/mysql-virtual-mailbox-domains.cf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
     mode    => 744,
-	content => $virtual_domains,
-	require => Class['::profile::mailserver::postfix'],
+    content => $virtual_domains,
+    require => Class['::profile::mailserver::postfix'],
   }
-  file { "/etc/postfix/mysql-virtual-mailbox-maps.cf":
-    owner   => "root",
-    group   => "root",
-	ensure  => "file",
+  file { '/etc/postfix/mysql-virtual-mailbox-maps.cf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
     mode    => 744,
-	content => $virtual_mailbox,
-	require => Class['::profile::mailserver::postfix'],
+    content => $virtual_mailbox,
+    require => Class['::profile::mailserver::postfix'],
   }
-  file { "/etc/postfix/mysql-virtual-alias-maps.cf":
-    owner   => "root",
-    group   => "root",
-	ensure  => "file",
+  file { '/etc/postfix/mysql-virtual-alias-maps.cf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
     mode    => 744,
-	content => $virtual_alias,
-	require => Class['::profile::mailserver::postfix'],
+    content => $virtual_alias,
+    require => Class['::profile::mailserver::postfix'],
   }
-  file { "/etc/postfix/mysql-virtual-email2email.cf":
-    owner   => "root",
-    group   => "root",
-	ensure  => "file",
+  file { '/etc/postfix/mysql-virtual-email2email.cf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
     mode    => 744,
-	content => $virtual_email2email,
-	require => Class['::profile::mailserver::postfix'],
+    content => $virtual_email2email,
+    require => Class['::profile::mailserver::postfix'],
   }
 
-  file { "/var/local/maildb-initial.sql":
-    owner => "root",
-    group => "root",
-    mode  => "644",
-    source => "puppet:///modules/profile/initial/maildb.sql",
+  file { '/var/local/maildb-initial.sql':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '644',
+    source => 'puppet:///modules/profile/initial/maildb.sql',
   } ->
-  mysql::db { 'mydb':
-    user     => ${mysql_user},
-    password => ${mysql_pass},
-    host     => ${mysql_host},
-    grant    => ['SELECT', 'UPDATE'],
-    sql      => '/var/local/maildb-initial.sql',
+  mysql::db { $mysql_name:
+    user           => $mysql_user,
+    password       => $mysql_pass,
+    host           => $mysql_host,
+    grant          => ['SELECT', 'UPDATE'],
+    sql            => '/var/local/maildb-initial.sql',
     import_timeout => 900,
-	require => Class['::mysql::server'],
+    require        => Class['::mysql::server'],
   }
 }
