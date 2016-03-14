071dd9e124d8f31672c089701bcce2110842a90a
Eigil Obrestad <eigil-github@obrestad.org>
2016-03-14 17:21:19 +0100

    Initial mailserver

--- manifests/mailserver/firewall.pp
+++ manifests/mailserver/firewall.pp
@@ -1,7 +1,7 @@
 class profile::mailserver::firewall {
   firewall { '010 accept incoming SMTP':
     proto  => 'tcp',
-	dport  => [25, 587],
+    dport  => [25, 587],
     action => 'accept',
   }
 }
