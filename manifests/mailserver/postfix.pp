76afd8fc1409e6e02058c86fd6f02e19f4e3e70d
Eigil Obrestad <eigil-github@obrestad.org>
2016-03-15 22:06:28 +0100

    Enable TLS for mailserver

--- manifests/mailserver/postfix.pp
+++ manifests/mailserver/postfix.pp
@@ -1,4 +1,6 @@
 class profile::mailserver::postfix {
+  $mailname = hiera('profile::mail::hostname')
+
   class { '::postfix':
     master_smtp => 'smtp inet n - n - - smtpd',
     master_smtps => 'smtps inet n - n - - smtpd',
@@ -9,5 +11,11 @@ class profile::mailserver::postfix {
   postfix::config { 'myhostname':
     ensure  => present,
     value   => $::fqdn,
+  }
+
+  postfix::config {
+    'smtpd_tls_cert_file':       value  => "/etc/letsencrypt/live/${mailname}/fullchain.pem";
+    'smtpd_tls_key_file':        value  => "/etc/letsencrypt/live/${mailname}/privkey.pem";
+    'smtpd_tls_security_level':  value  => 'may';
   }
 }
