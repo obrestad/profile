f9b3c8233fa064f002f0ea53b9e0f8b39bf646a7
Eigil Obrestad <eigil-github@obrestad.org>
2016-03-15 22:56:30 +0100

    Syntax

--- manifests/mailserver/postfix.pp
+++ manifests/mailserver/postfix.pp
@@ -2,29 +2,35 @@ class profile::mailserver::postfix {
   $mailname = hiera('profile::mail::hostname')
 
   class { '::postfix':
-    master_smtp => 'smtp inet n - n - - smtpd',
-    master_smtps => 'smtps inet n - n - - smtpd',
+    master_smtp       => 'smtp inet n - n - - smtpd',
+    master_smtps      => 'smtps inet n - n - - smtpd',
     master_submission => 'submission inet n - n - - smtpd',
-    mta => true,
-	relayhost => 'direct',
+    mta               => true,
+    relayhost         => 'direct',
   }
 
   # Various settings
-  postfix::config { 
+  postfix::config {
     'myhostname':          value => $::fqdn;
-	'recipient_delimiter': value => '-';
+    'recipient_delimiter': value => '-';
   }
 
   # Deny relay access etc.
   postfix::config {
-    'smtpd_recipient_restrictions': value => 'permit_sasl_authenticated, permit_mynetworks, defer_unauth_destination';
-    'smtpd_relay_restrictions': value => 'permit_sasl_authenticated, permit_mynetworks, defer_unauth_destination';
+    'smtpd_recipient_restrictions':
+      value => 'permit_sasl_authenticated, permit_mynetworks, \
+      defer_unauth_destination';
+    'smtpd_relay_restrictions':
+      value => 'permit_sasl_authenticated, permit_mynetworks, \
+      defer_unauth_destination';
   }
 
   # Implement TLS
   postfix::config {
-    'smtpd_tls_cert_file':       value  => "/etc/letsencrypt/live/${::fqdn}/fullchain.pem";
-    'smtpd_tls_key_file':        value  => "/etc/letsencrypt/live/${::fqdn}/privkey.pem";
+    'smtpd_tls_cert_file':
+      value  => "/etc/letsencrypt/live/${::fqdn}/fullchain.pem";
+    'smtpd_tls_key_file':
+      value  => "/etc/letsencrypt/live/${::fqdn}/privkey.pem";
     'smtpd_tls_security_level':  value  => 'may';
   }
 }
