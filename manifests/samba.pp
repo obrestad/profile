9d1441d3805bf396f92fe385195756d94d2a1ded
Eigil Obrestad <eigil-github@obrestad.org>
2016-04-23 14:08:13 +0200

    Initial samba

---
+++ manifests/samba.pp
@@ -0,0 +1,28 @@
+class profile::samba {
+  $samba_eigil_pass = hiera('profile::user::eigil::samba::pass')
+
+  class {'samba::server':
+    workgroup     => 'rothaugane',
+    server_string => 'Antoccino',
+    interfaces    => 'eth0 lo',
+    security      => 'share'
+  }
+
+  samba::server::user { 'eigil':
+    password = $samba_eigil_pass,
+  }
+
+  samba::server::share {'archive':
+    comment              => 'Archived stuff',
+    path                 => '/srv/archive',
+    guest_only           => false,
+    guest_ok             => flase,
+    guest_account        => 'nobody',
+    browsable            => true,
+    create_mask          => 0777,
+    force_create_mask    => 0777,
+    directory_mask       => 0777,
+    force_directory_mask => 0777,
+    force_group          => 'users',
+  }
+}
