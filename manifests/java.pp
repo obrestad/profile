64ff367691ed73478571220a240ca440f209eca3
Eigil Obrestad <eigil-github@obrestad.org>
2016-04-28 00:52:31 +0200

    Hrmpf

--- manifests/java.pp
+++ manifests/java.pp
@@ -1,13 +1,13 @@
 class profile::java {
-#  class { 'java':
-#    distribution => 'sun-jdk',
-#  }
-
-  java::oracle { 'jdk8' :
-    ensure  => 'present',
-    version => '8',
-    java_se => 'jdk',
+  class { 'java':
+    distribution => 'jdk',
   }
+
+#  java::oracle { 'jdk8' :
+#    ensure  => 'present',
+#    version => '8',
+#    java_se => 'jdk',
+#  }
 
   firewall { '015 accept incoming Minecraft':
     proto  => 'tcp',
