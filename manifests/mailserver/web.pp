a611dba44e7d87f2a62615a07c18bee780cfa4bc
Eigil Obrestad <eigil-github@obrestad.org>
2016-07-12 13:59:55 +0200

    Configures roundcube

--- manifests/mailserver/web.pp
+++ manifests/mailserver/web.pp
@@ -7,6 +7,7 @@ class profile::mailserver::web {
   $mysql_host = hiera('profile::mail::web::db::host')
   $mysql_user = hiera('profile::mail::web::db::user')
   $mysql_pass = hiera('profile::mail::web::db::pass')
+  $rc_des_key = hiera('profile::mail::web::deskey')
 
   apache::vhost { "${mailname} http":
     servername    => $mailname,
@@ -62,7 +63,87 @@ class profile::mailserver::web {
     ensure  => present,
     path    => '/etc/roundcube/config.inc.php',
     setting => '$config[\'default_host\']',
-    value   => '\'mail.rothaugane.com\'',
+    value   => "'ssl://${mailname}:993'",
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube SMTP Host':
+    ensure  => present,
+    path    => '/etc/roundcube/config.inc.php',
+    setting => '$config[\'smtp_server\']',
+    value   => "'${mailname}'",
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube SMTP Port':
+    ensure  => present,
+    path    => '/etc/roundcube/config.inc.php',
+    setting => '$config[\'smtp_port\']',
+    value   => '\'587\'',
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube SMTP User':
+    ensure  => present,
+    path    => '/etc/roundcube/config.inc.php',
+    setting => '$config[\'smtp_user\']',
+    value   => '\'%u\'',
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube SMTP Pass':
+    ensure  => present,
+    path    => '/etc/roundcube/config.inc.php',
+    setting => '$config[\'smtp_pass\']',
+    value   => '\'%p\'',
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube DES Key':
+    ensure  => present,
+    path    => '/etc/roundcube/config.inc.php',
+    setting => '$config[\'des_key\']',
+    value   => "'${rc_des_key}'",
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube DB Host':
+    ensure  => present,
+    path    => '/etc/roundcube/debian-db.php',
+    setting => '$dbserver',
+    value   => "'${mysql_host}'",
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube DB Name':
+    ensure  => present,
+    path    => '/etc/roundcube/debian-db.php',
+    setting => '$dbname',
+    value   => "'${mysql_name}'",
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube DB User':
+    ensure  => present,
+    path    => '/etc/roundcube/debian-db.php',
+    setting => '$dbserver',
+    value   => "'${mysql_user}'",
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube DB Pass':
+    ensure  => present,
+    path    => '/etc/roundcube/debian-db.php',
+    setting => '$dbserver',
+    value   => "'${mysql_pass}'",
+    require => Package['roundcube'],
+  }
+
+  ini_setting { 'Roundcube DB Type':
+    ensure  => present,
+    path    => '/etc/roundcube/debian-db.php',
+    setting => '$dbtype',
+    value   => '\'mysql\'',
     require => Package['roundcube'],
   }
 }
