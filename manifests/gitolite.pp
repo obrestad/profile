3c70d33cdfd55744dc081bd83765d7e1759cc05e
Eigil Obrestad <eigil-github@obrestad.org>
2016-07-29 21:06:38 +0200

    Initial installation of gitolite

---
+++ manifests/gitolite.pp
@@ -0,0 +1,36 @@
+# This class installs gitolite to allow for hosting git repos with access
+# control.
+class profile::gitolite {
+  $admin_key = hiera('profile::gitolite::admin-key')
+
+  package{ 'gitolite3':
+    ensure => present,
+  }
+
+  user { 'git':
+    ensure      => present,
+    gid         => 'service',
+    require     => Group['service'],
+    shell       => '/bin/bash',
+    home        => '/srv/git',
+    managehome  => true,
+    system      => true,
+  }
+
+  file { '/srv/git/admin_pub_key':
+    ensure   => 'file',
+    owner    => 'git',
+    group    => 'service',
+    mode     => '0644',
+    require  => User['git'],
+    content  => $admin_key,
+  }
+
+  exec { 'gitolite setup -pk admin_pub_key':
+    cwd     => '/srv/git',
+    path    => '/usr/bin',
+    user    => 'git',
+    creates => '/srv/git/.gitolite.rc',
+    require => File['/srv/git/admin_pub_key'],
+  }
+}
