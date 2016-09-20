8924f5993f06fc7d1c30899c7161788fa7c8ee2e
Eigil Obrestad <eigil-github@obrestad.org>
2016-09-20 20:47:13 +0200

    Initial class for unify controller

---
+++ manifests/ubnt.pp
@@ -0,0 +1,24 @@
+class profile::ubnt {
+  apt::source { 'ubnt':
+    location   => 'http://www.ubnt.com/downloads/unifi/debian',
+    repos      => 'ubiquiti',
+    release    => 'stable',
+    key        => 'C0A52C50',
+    key_server => 'keyserver.ubuntu.com',
+  }
+
+  firewall { '011 accept incoming UAPs and management':
+    proto   => 'tcp',
+    dport   => [8080, 8443],
+    iniface => 'eth0',
+    action  => 'accept',
+  }
+
+  firewall { '011 v6 accept incoming UAPs and management':
+    proto    => 'tcp',
+    dport    => [8080, 8443],
+    action   => 'accept',
+    iniface  => 'eth0',
+    provider => 'ip6tables',
+  }
+}
