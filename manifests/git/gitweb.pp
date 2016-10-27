196035100edc6a56bd11b68190bc94fb24493652
Eigil Obrestad <eigil-github@obrestad.org>
2016-10-27 19:12:04 +0200

    Add apache vhost for the gitweb page

--- manifests/git/gitweb.pp
+++ manifests/git/gitweb.pp
@@ -1,6 +1,30 @@
 # This class installs gitweb.
 class profile::git::gitweb {
+  $gitwebname = hiera('profile::git::gitweb')
+
   package{ 'gitweb':
     ensure => present,
+  }
+
+  apache::vhost { "${gitwebname} http":
+    servername    => $gitwebname,
+    port          => '80',
+    docroot       => '/srv/git/repositories/',
+    docroot_owner => 'git',
+    docroot_group => 'service',
+    setenv        => ['GITWEB_CONFIG /srv/git/gitweb.conf'],
+    directories   => [
+      { path           => '/srv/git/repositories/',
+        options        => ['Indexes', 'FollowSymlinks', 'ExecCGI'],
+        allow_override => ['None'],
+        require        => 'all granted',
+      },
+    ],
+    rewrites      => [
+      { rewrite_rule => [ '^/$  /cgi-bin/gitweb.cgi' ]
+      },
+      { rewrite_rule => [ '^/(.*\.git/(?!/?(HEAD|info|objects|refs)).*)?$ /cgi-bin/gitweb.cgi%{REQUEST_URI}  [L,PT]' ]
+      },
+    ],
   }
 }
