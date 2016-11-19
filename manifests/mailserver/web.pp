502f3eec09b23373698cba31704e05ffca5bef72
Eigil Obrestad <eigil-github@obrestad.org>
2016-11-19 22:57:04 +0100

    staticfiles has not moved...

--- manifests/mailserver/web.pp
+++ manifests/mailserver/web.pp
@@ -44,7 +44,7 @@ class profile::mailserver::web {
     wsgi_process_group          => 'mailadmin',
     aliases                     => [
       { alias   => '/static/',
-        path    => "${location}/mailadminstatic/",
+        path    => '/opt/mailadminstatic/',
       },
     ],
   }
@@ -85,7 +85,7 @@ class profile::mailserver::web {
     wsgi_process_group          => 'mailadmin-ssl',
     aliases                     => [
       { alias   => '/static/',
-        path    => "${location}/mailadminstatic/",
+        path    => '/opt/mailadminstatic/',
       },
     ],
   }
