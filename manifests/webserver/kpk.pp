6d626568027db5d637b4770a784c1bc047e3a6a2
Eigil Obrestad <eigil-github@obrestad.org>
2016-11-24 21:51:15 +0100

    Too much LaTeX

--- manifests/webserver/kpk.pp
+++ manifests/webserver/kpk.pp
@@ -9,20 +9,20 @@ class profile::webserver::kpk {
     docroot_group => 'www-data',
   }
 
-  %apache::vhost { "${::fqdn} https":
-  %  servername    => $::fqdn,
-  %  port          => '443',
-  %  docroot       => "/var/www/${::fqdn}",
-  %  ssl           => true,
-  %  ssl_cert      => "/etc/letsencrypt/live/${::fqdn}/fullchain.pem",
-  %  ssl_key       => "/etc/letsencrypt/live/${::fqdn}/privkey.pem",
-  %}
+  #apache::vhost { "${::fqdn} https":
+  #  servername    => $::fqdn,
+  #  port          => '443',
+  #  docroot       => "/var/www/${::fqdn}",
+  #  ssl           => true,
+  #  ssl_cert      => "/etc/letsencrypt/live/${::fqdn}/fullchain.pem",
+  #  ssl_key       => "/etc/letsencrypt/live/${::fqdn}/privkey.pem",
+  #}
 
-  %letsencrypt::certonly { $::fqdn:
-  %  domains       => [$::fqdn],
-  %  plugin        => 'webroot',
-  %  webroot_paths => ["/var/www/${::fqdn}"],
-  %  require       => Apache::Vhost["${::fqdn} http"],
-  %  manage_cron   => true,
-  %}
+  #letsencrypt::certonly { $::fqdn:
+  #  domains       => [$::fqdn],
+  #  plugin        => 'webroot',
+  #  webroot_paths => ["/var/www/${::fqdn}"],
+  #  require       => Apache::Vhost["${::fqdn} http"],
+  #  manage_cron   => true,
+  #}
 }
