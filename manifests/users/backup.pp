5a8847b7833a510dbe84c80a00ac045b19badf05
Eigil Obrestad <eigil-github@obrestad.org>
2016-11-24 23:09:23 +0100

    Create www folder in eigils home, and puppet-linting.

--- manifests/users/backup.pp
+++ manifests/users/backup.pp
@@ -1,3 +1,4 @@
+# Create a user which can be used for backups
 class profile::users::backup {
   user { 'remote-backup':
     ensure      => present,
@@ -7,34 +8,33 @@ class profile::users::backup {
     shell       => '/bin/bash',
     home        => '/home/remote-backup',
     managehome  => true,
-    password    => "*",
+    password    => '*',
   }
-  
-  file { "/home/remote-backup/.ssh":
-    owner    => "remote-backup",
-    group    => "service",
-    mode     => "700",
-    ensure   => "directory",
+
+  file { '/home/remote-backup/.ssh':
+    ensure   => 'directory',
+    owner    => 'remote-backup',
+    group    => 'service',
+    mode     => '0700',
     require  => User['remote-backup'],
   }
 
-  ssh_authorized_key { "root@antoccino":
-    user => "remote-backup",
-    type => 'ssh-rsa',
-    key => 	'AAAAB3NzaC1yc2EAAAADAQABAAABAQC/OdpRVK/mISztiaxquAz1R8O/eR4r30vU77QmERzQyNM4Kx90QWYR91vQZ3gm0cb3DtHeqvRL6xBROTGY1OeTBOd4GZYBQ2VOVSTdQoxmnxl8nKkx7B4eAryXoXAbuWOWqHIoAxow5RbopzO3HJ+rCPAS5n2m6AJleUAVpEwN8uqg4iyEOZa/aT4QvSm6qahzNG+k7eX3i0qarm4Jc6GoJLBP17lt0lLf4/ortXD9yoni66jtS/dXUXacFVHJQfolCfO30W3R9+yYY93n8GHRx/J8LNB/If6e5xBxt3FHxXgyMO7hnwPm0Chn3kC5tXN25aPow+a5w+s2Lk7VMue3',
-    require => File['/home/remote-backup/.ssh'], 
+  ssh_authorized_key { 'root@antoccino':
+    user    => 'remote-backup',
+    type    => 'ssh-rsa',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC/OdpRVK/mISztiaxquAz1R8O/eR4r30vU77QmERzQyNM4Kx90QWYR91vQZ3gm0cb3DtHeqvRL6xBROTGY1OeTBOd4GZYBQ2VOVSTdQoxmnxl8nKkx7B4eAryXoXAbuWOWqHIoAxow5RbopzO3HJ+rCPAS5n2m6AJleUAVpEwN8uqg4iyEOZa/aT4QvSm6qahzNG+k7eX3i0qarm4Jc6GoJLBP17lt0lLf4/ortXD9yoni66jtS/dXUXacFVHJQfolCfO30W3R9+yYY93n8GHRx/J8LNB/If6e5xBxt3FHxXgyMO7hnwPm0Chn3kC5tXN25aPow+a5w+s2Lk7VMue3',
+    require => File['/home/remote-backup/.ssh'],
   }
-  ssh_authorized_key { "root@frappuccino":
-    user => "remote-backup",
-    type => 'ssh-rsa',
-    key =>	'AAAAB3NzaC1yc2EAAAADAQABAAABAQDMLO39rOuIs20ndBn/nGFjYlYIj1RVyYr/UJMAVUSMbAT1iFkWZ/CL2GLHTw/mIGJuqFJFYVgzMX8Q/RXT9qAXqKBmc7yed2IEh18qC4Q3Gz0DkUf9znL4PFo+UjQ6+QSf2pXG3Sj7Fm44hI+2/2cb6IEP/8UhSy8287WAQrgGiocoqPmCwiy/MGVwMF41oCd6tQU5LoqRka2MWSNBo01wSIIjhHRejp6FxMfz79JEl/eddHGL/8A0PtZrBS/WIoIPorKuhVXZVV9NpxXdGMlKONBndKd67OvM+zV0RmuNSNqqGC5v6TWLjeuTJIaIy7Mo32XHR+E+JwqLHMbExZn3',
-    require => File['/home/remote-backup/.ssh'], 
+  ssh_authorized_key { 'root@frappuccino':
+    user    => 'remote-backup',
+    type    => 'ssh-rsa',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDMLO39rOuIs20ndBn/nGFjYlYIj1RVyYr/UJMAVUSMbAT1iFkWZ/CL2GLHTw/mIGJuqFJFYVgzMX8Q/RXT9qAXqKBmc7yed2IEh18qC4Q3Gz0DkUf9znL4PFo+UjQ6+QSf2pXG3Sj7Fm44hI+2/2cb6IEP/8UhSy8287WAQrgGiocoqPmCwiy/MGVwMF41oCd6tQU5LoqRka2MWSNBo01wSIIjhHRejp6FxMfz79JEl/eddHGL/8A0PtZrBS/WIoIPorKuhVXZVV9NpxXdGMlKONBndKd67OvM+zV0RmuNSNqqGC5v6TWLjeuTJIaIy7Mo32XHR+E+JwqLHMbExZn3',
+    require => File['/home/remote-backup/.ssh'],
   }
-  ssh_authorized_key { "b-eigil@carajillo":
-    user => "remote-backup",
-    type => 'ssh-rsa',
-    key =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDwe4N6Op3OEDYxe/SeHr58jgq7/Ip7uSDLYuOtJl40/IHVWyCwMfQwFWgIzNM+8Obpu9uRvwp85hF7PHoM5MTNgcPGhlJeFUkHbiUu3fhlj4k+YmiW9NpotNbVbTw0s3m2PLVruEvVm8fQ376XTJO2jTOfx7DC25dV+UqAe7XtmZra6l2wEZPr2UN5W7TGr3Th19dkBiQBQMxIGFd/toKOniFdq/+JBp6OH+ZMQ8QIgKkAQ/AKCxYROkxYvWblvOtjk0kehaJkn3b6wjgyMf80yxLvOd4jaX7LO/G8kXjXQ+1gwVImvG0Cw4yG5D20Y+UspwahXeRV5Ik72qsF3ezX',
-    require => File['/home/remote-backup/.ssh'], 
+  ssh_authorized_key { 'b-eigil@carajillo':
+    user    => 'remote-backup',
+    type    => 'ssh-rsa',
+    key     =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDwe4N6Op3OEDYxe/SeHr58jgq7/Ip7uSDLYuOtJl40/IHVWyCwMfQwFWgIzNM+8Obpu9uRvwp85hF7PHoM5MTNgcPGhlJeFUkHbiUu3fhlj4k+YmiW9NpotNbVbTw0s3m2PLVruEvVm8fQ376XTJO2jTOfx7DC25dV+UqAe7XtmZra6l2wEZPr2UN5W7TGr3Th19dkBiQBQMxIGFd/toKOniFdq/+JBp6OH+ZMQ8QIgKkAQ/AKCxYROkxYvWblvOtjk0kehaJkn3b6wjgyMf80yxLvOd4jaX7LO/G8kXjXQ+1gwVImvG0Cw4yG5D20Y+UspwahXeRV5Ik72qsF3ezX',
+    require => File['/home/remote-backup/.ssh'],
   }
-  
 }
--- manifests/users/eigil.pp
+++ manifests/users/eigil.pp
@@ -1,3 +1,4 @@
+# Create a user for eigil
 class profile::users::eigil {
   user { 'eigil':
     ensure      => present,
@@ -8,47 +9,55 @@ class profile::users::eigil {
     shell       => '/bin/bash',
     home        => '/home/eigil',
     managehome  => true,
-    password    => hiera("profile::user::eigil::hash"),
+    password    => hiera('profile::user::eigil::hash'),
   }
-  
-  file { "/home/eigil/.bashrc":
-    owner    => "eigil",
-    group    => "users",
-    mode  => "440",
-    source => "puppet:///modules/profile/userpref/bashrc",
-  }   
-  file { "/home/eigil/.vimrc":
-    owner    => "eigil",
-    group    => "users",
-    mode  => "440",
-    source => "puppet:///modules/profile/userpref/vimrc",
+
+  file { '/home/eigil/.bashrc':
+    owner   => 'eigil',
+    group   => 'users',
+    mode    => '0440',
+    source  => 'puppet:///modules/profile/userpref/bashrc',
   }
-  file { "/home/eigil/.ssh":
-    owner    => "eigil",
-    group    => "users",
-    mode     => "700",
-    ensure   => "directory",
+  file { '/home/eigil/.vimrc':
+    owner   => 'eigil',
+    group   => 'users',
+    mode    => '0440',
+    source  => 'puppet:///modules/profile/userpref/vimrc',
+  }
+  file { '/home/eigil/.ssh':
+    ensure  => 'directory',
+    owner   => 'eigil',
+    group   => 'users',
+    mode    => '0700',
+    require => User['eigil'],
+  }
+
+  file { '/home/eigil/www':
+    ensure   => 'directory',
+    owner    => 'eigil',
+    group    => 'www-data',
+    mode     => '2755',
     require  => User['eigil'],
   }
-  
-  ssh_authorized_key { "eigil@mocha":
-    user => "eigil",
-    type => 'ssh-rsa',
-    key => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAvZKZc91/Ga62TYSdGyDd+nbFwyYUwb23bye2XPOC30K/KsokMCemxxxQ2Y+1XmL8AZ2QvzVwvuN/DFpF9km54v9cZtjuby4VcJRGJzXoCTsXmshGn6JhIGKJ1CdeOU4LFLaDJIchugHk4D77XBmOB/4LPe8OqGEMQdRgRYolYTs708Mv/NHxKu/cp5o0dsRoEObUgaIcG5jfA1WnpRb4fWdX5NL/waOidGjO3GTNYOUH8iY8qZVZiW/QxTLxpIDwFtF9U1Eu6Kl/DPT/+0VaV0+2R7q30ZA5csdUkfdvxliMiu7kcFKI5HrAMcSgkrK/MGUWo5wXMCom7U9MezlC2Q==',
-    require => File['/home/eigil/.ssh'], 
+
+  ssh_authorized_key { 'eigil@mocha':
+    user    => 'eigil',
+    type    => 'ssh-rsa',
+    key     => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAvZKZc91/Ga62TYSdGyDd+nbFwyYUwb23bye2XPOC30K/KsokMCemxxxQ2Y+1XmL8AZ2QvzVwvuN/DFpF9km54v9cZtjuby4VcJRGJzXoCTsXmshGn6JhIGKJ1CdeOU4LFLaDJIchugHk4D77XBmOB/4LPe8OqGEMQdRgRYolYTs708Mv/NHxKu/cp5o0dsRoEObUgaIcG5jfA1WnpRb4fWdX5NL/waOidGjO3GTNYOUH8iY8qZVZiW/QxTLxpIDwFtF9U1Eu6Kl/DPT/+0VaV0+2R7q30ZA5csdUkfdvxliMiu7kcFKI5HrAMcSgkrK/MGUWo5wXMCom7U9MezlC2Q==',
+    require => File['/home/eigil/.ssh'],
   }
-  
-  ssh_authorized_key { "eigil@carajillo":
-    user => "eigil",
-    type => 'ssh-rsa',
-    key =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDwe4N6Op3OEDYxe/SeHr58jgq7/Ip7uSDLYuOtJl40/IHVWyCwMfQwFWgIzNM+8Obpu9uRvwp85hF7PHoM5MTNgcPGhlJeFUkHbiUu3fhlj4k+YmiW9NpotNbVbTw0s3m2PLVruEvVm8fQ376XTJO2jTOfx7DC25dV+UqAe7XtmZra6l2wEZPr2UN5W7TGr3Th19dkBiQBQMxIGFd/toKOniFdq/+JBp6OH+ZMQ8QIgKkAQ/AKCxYROkxYvWblvOtjk0kehaJkn3b6wjgyMf80yxLvOd4jaX7LO/G8kXjXQ+1gwVImvG0Cw4yG5D20Y+UspwahXeRV5Ik72qsF3ezX',
-    require => File['/home/eigil/.ssh'], 
+
+  ssh_authorized_key { 'eigil@carajillo':
+    user    => 'eigil',
+    type    => 'ssh-rsa',
+    key     =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDwe4N6Op3OEDYxe/SeHr58jgq7/Ip7uSDLYuOtJl40/IHVWyCwMfQwFWgIzNM+8Obpu9uRvwp85hF7PHoM5MTNgcPGhlJeFUkHbiUu3fhlj4k+YmiW9NpotNbVbTw0s3m2PLVruEvVm8fQ376XTJO2jTOfx7DC25dV+UqAe7XtmZra6l2wEZPr2UN5W7TGr3Th19dkBiQBQMxIGFd/toKOniFdq/+JBp6OH+ZMQ8QIgKkAQ/AKCxYROkxYvWblvOtjk0kehaJkn3b6wjgyMf80yxLvOd4jaX7LO/G8kXjXQ+1gwVImvG0Cw4yG5D20Y+UspwahXeRV5Ik72qsF3ezX',
+    require => File['/home/eigil/.ssh'],
   }
-  
-  ssh_authorized_key { "eigil@ristretto":
-    user => "eigil",
-    type => 'ssh-rsa',
-    key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCeqaOcZY9UWzQVTb+g9aArTP/VCdhdc3jfekbCPWsEaZMiSMEcKGkmnvzHqj9D4VeiqiKWYMJOkM1/KzK+6eh+HgChIR3D2iiM2K3jR060xWzD33hPMzR/d6aF2Iy+ZVaQG+m3rcAMzjBdWqDMechkKyGyc6/rmI/lQwtVfeDqUSYMbsRQ2Lr9uNLCvW0o91rcr46NlD5a/kSRNCydNbOcA19eAnOSxWohDZ45U8iy2lem+6C7Uy80Hs4N0F3Brrn8we8yw7gWeS+W1ElsFV8KpznFQF33phMdkH2H9UzaaWMQo2CIlMf57vsYZzUfbvJDmGszCQrc0BUQ2WPhsgfh",
-    require => File['/home/eigil/.ssh'], 
+
+  ssh_authorized_key { 'eigil@ristretto':
+    user    => 'eigil',
+    type    => 'ssh-rsa',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCeqaOcZY9UWzQVTb+g9aArTP/VCdhdc3jfekbCPWsEaZMiSMEcKGkmnvzHqj9D4VeiqiKWYMJOkM1/KzK+6eh+HgChIR3D2iiM2K3jR060xWzD33hPMzR/d6aF2Iy+ZVaQG+m3rcAMzjBdWqDMechkKyGyc6/rmI/lQwtVfeDqUSYMbsRQ2Lr9uNLCvW0o91rcr46NlD5a/kSRNCydNbOcA19eAnOSxWohDZ45U8iy2lem+6C7Uy80Hs4N0F3Brrn8we8yw7gWeS+W1ElsFV8KpznFQF33phMdkH2H9UzaaWMQo2CIlMf57vsYZzUfbvJDmGszCQrc0BUQ2WPhsgfh',
+    require => File['/home/eigil/.ssh'],
   }
 }
