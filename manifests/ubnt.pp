cda5612b48193b5475b7bf8df52fcc630e3967a9
Eigil Obrestad <eigil-git@obrestad.org>
2017-01-20 16:43:10 +0100

    Added backup of unifi controller, and a vhost for the unifi url

--- manifests/ubnt.pp
+++ manifests/ubnt.pp
@@ -1,5 +1,7 @@
 # Installs a ubnt controller
 class profile::ubnt {
+  $unifiurl = hiera('profile::unifi::url')
+
   apt::source { 'ubnt':
     location   => 'http://www.ubnt.com/downloads/unifi/debian',
     repos      => 'ubiquiti',
@@ -23,13 +25,51 @@ class profile::ubnt {
     dport   => [8080, 8443],
     iniface => 'eth0',
     action  => 'accept',
-  }
-
+  }49
   firewall { '011 v6 accept incoming UAPs and management':
     proto    => 'tcp',
     dport    => [8080, 8443],
     action   => 'accept',
     iniface  => 'eth0',
     provider => 'ip6tables',
+  }
+
+  apache::vhost { '${unifiurl} http':
+    servername    => $unifiurl,
+    serveraliases => [$unifiurl],
+    port          => '80',
+    docroot       => "/var/www/${unifiurl}",
+    docroot_owner => 'www-data',
+    docroot_group => 'www-data',
+  }
+
+  letsencrypt::certonly { $unifiurl:
+    domains       => [$unifiurl],
+    plugin        => 'webroot',
+    webroot_paths => ["/var/www/${unifiurl}"],
+    require       => Apache::Vhost["${unifiurl} http"],
+    manage_cron   => true,
+  }
+
+  apache::vhost { "${unifiurl} https":
+    servername    => $unifiurl,
+    port          => '443',
+    docroot       => "/var/www/${unifiurl}",
+    ssl           => true,
+    ssl_cert      => "/etc/letsencrypt/live/${unifiurl}/fullchain.pem",
+    ssl_key       => "/etc/letsencrypt/live/${unifiurl}/privkey.pem",
+  }
+
+  file { '/usr/local/sbin/unifi-backup':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0744',
+    source => 'puppet:///modules/profile/scripts/unifi-backup.sh',
+  }->
+  cron { 'unifi-backup':
+    command => '/usr/local/sbin/unifi-backup',
+    user    => root,
+    hour    => [3, 9, 15, 21],
+    minute  => [49],
   }
 }
