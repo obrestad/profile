49d6827efbbf2071ac09e9f26769d57088d6f36a
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-02 23:35:10 +0100

    Attempt to fix munin graphing

--- manifests/munin/master.pp
+++ manifests/munin/master.pp
@@ -3,6 +3,9 @@ class profile::munin::master {
   $munin_url = hiera('profile::munin::url')
 
   class{ '::munin::master':
+    extra_config => [
+      'cgiurl_graph /munin-cgi/munin-cgi-graph',
+    ],
   }
 
   apache::vhost { "${munin_url} http":
@@ -13,6 +16,17 @@ class profile::munin::master {
     docroot_owner => 'www-data',
     docroot_group => 'www-data',
     directories   => [
+      { path            => '/munin-cgi/munin-cgi-html',
+        provider        => 'location',
+        require         => 'all granted',
+        custom_fragment => '
+      <IfModule mod_fcgid.c>
+        SetHandler fcgid-script
+      </IfModule>
+      <IfModule !mod_fcgid.c>
+        SetHandler cgi-script
+      </IfModule>',
+      },
       { path            => '/munin-cgi/munin-cgi-graph',
         provider        => 'location',
         require         => 'all granted',
@@ -28,10 +42,38 @@ class profile::munin::master {
         require => 'all granted',
         options => ['None'],
       },
+      { path    => '/var/cache/munin/www/static',
+        require => 'all granted',
+        options => ['None'],
+      },
     ],
     aliases       => [
+      { scriptalias => '/munin-cgi/munin-cgi-html',
+        path        => '/usr/lib/munin/cgi/munin-cgi-html',
+      },
       { scriptalias => '/munin-cgi/munin-cgi-graph',
         path        => '/usr/lib/munin/cgi/munin-cgi-graph',
+      },
+    ],
+    rewrites       => [
+      { rewrite_rule => [
+          '^/favicon.ico /var/cache/munin/www/static/favicon.ico [L]',
+        ]
+      },
+      { rewrite_rule => [
+          '^/static/(.*) /var/cache/munin/www/static/$1          [L]',
+        ]
+      },
+      { rewrite_rule => [
+          '^/munin-cgi/munin-cgi-graph/(.*) /$1',
+        ]
+      },
+      { rewrite_cond => [
+          '%{REQUEST_URI}                 !^/static',
+        ]
+        rewrite_rule => [
+          'RewriteRule ^/(.*.png)$  /munin-cgi/munin-cgi-graph/$1 [L,PT]',
+        ]
       },
     ],
   }
