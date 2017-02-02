73a1ab033fd7bc16aa8a9b44f3ef321af0c2cbe0
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-02 23:49:38 +0100

    Allow connections from localhost

--- manifests/munin/node.pp
+++ manifests/munin/node.pp
@@ -1,6 +1,6 @@
 # Installs the munin node
 class profile::munin::node {
   class{ '::munin::node':
-    allow => [ '2a01:79d:469c:429c::/64' ]
+    allow => [ '::ffff:127.0.0.1/128', '2a01:79d:469c:429c::/64' ]
   }
 }
