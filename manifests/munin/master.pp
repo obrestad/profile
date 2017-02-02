31a0006f860cabe08dfe6f66a224441dd9d40bfb
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-02 22:56:41 +0100

    Create a vhost for munin

--- manifests/munin/master.pp
+++ manifests/munin/master.pp
@@ -1,5 +1,38 @@
 # Installs the munin master node
 class profile::munin::master {
+  $munin_url = hiera('profile::munin::url')
+
   class{ '::munin::master':
+  }
+
+  apache::vhost { "${munin_url} http":
+    servername    => $munin_url,
+    serveraliases => [$munin_url],
+    port          => '80',
+    docroot       => '/var/cache/munin/www',
+    docroot_owner => 'www-data',
+    docroot_group => 'www-data',
+    directories   => [
+      { path            => '/munin-cgi/munin-cgi-graph',
+        provider        => 'location',
+        require         => 'local',
+        custom_fragment => '
+      <IfModule mod_fcgid.c>
+        SetHandler fcgid-script
+      </IfModule>
+      <IfModule !mod_fcgid.c>
+        SetHandler cgi-script
+      </IfModule>',
+      },
+      { path    => '/var/cache/munin/www',
+        require => 'local',
+        options => ['None'],
+      },
+    ],
+    aliases       => [
+      { scriptalias => '/munin-cgi/munin-cgi-graph',
+        path        => '/usr/lib/munin/cgi/munin-cgi-graph',
+      },
+    ],
   }
 }
