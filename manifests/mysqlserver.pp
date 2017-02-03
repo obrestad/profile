6ad34f499c8f8fe85e18f2601f35fe995b12545b
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-03 13:10:08 +0100

    Syntax and linting

--- manifests/mysqlserver.pp
+++ manifests/mysqlserver.pp
@@ -1,144 +1,145 @@
+# Installs a simple mysql server, and configures munin monitoring of it.
 class profile::mysqlserver {
   class { '::mysql::server':
-    root_password           => hiera("profile::mysql::rootpw"),
+    root_password           => hiera('profile::mysql::rootpw'),
     remove_default_accounts => true,
   }
 
   munin::plugin { 'mysql_bin_relay_log':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_commands':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_connections':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_files_tables':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_bpool':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_bpool_act':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_insert_buf':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_io':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_io_pend':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_log':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_rows':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_semaphores':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_innodb_tnx':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_myisam_indexes':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_network_traffic':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_qcache':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_qcache_mem':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_replication':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_select_types':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_slow':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_sorts':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_table_locks':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
   munin::plugin { 'mysql_tmp_tables':
     ensure => link,
-    target => 'mysql_'
+    target => 'mysql_',
     config => ['user root'],
   }
 
