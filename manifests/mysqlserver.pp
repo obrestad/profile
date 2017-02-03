1d4d486c89a9347d1d356f966f55e7fd6dc011ae
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-03 19:13:50 +0100

    Add password for the mysql plugins

--- manifests/mysqlserver.pp
+++ manifests/mysqlserver.pp
@@ -1,146 +1,217 @@
 # Installs a simple mysql server, and configures munin monitoring of it.
 class profile::mysqlserver {
+  $mysql_root_pw = hiera('profile::mysql::rootpw')
+
   class { '::mysql::server':
-    root_password           => hiera('profile::mysql::rootpw'),
+    root_password           => $mysql_root_pw,
     remove_default_accounts => true,
   }
 
   munin::plugin { 'mysql_bin_relay_log':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_commands':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_connections':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_files_tables':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_bpool':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_bpool_act':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_insert_buf':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_io':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_io_pend':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_log':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_rows':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_semaphores':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_innodb_tnx':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_myisam_indexes':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_network_traffic':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_qcache':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_qcache_mem':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_replication':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_select_types':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_slow':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_sorts':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_table_locks':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_tmp_tables':
     ensure => link,
     target => 'mysql_',
-    config => ['user root'],
+    config => [
+      'user root',
+      "env.mysqlpassword ${mysql_root_pw}",
+    ],
   }
 
   munin::plugin { 'mysql_bytes':
