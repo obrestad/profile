5a8f5378a40479515d570095d3080501db81e412
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-03 10:47:25 +0100

    Only allow localhost munin node for now

--- manifests/munin/node.pp
+++ manifests/munin/node.pp
@@ -1,6 +1,6 @@
 # Installs the munin node
 class profile::munin::node {
   class{ '::munin::node':
-    allow => [ '::ffff:127.0.0.1/128', '2a01:79d:469c:429c::/64' ]
+    allow => [ '127\.0\.0\.1' ]
   }
 }
