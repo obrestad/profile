30df70412c6c816f24fb19c14525c7644acf8ffc
Eigil Obrestad <eigil-git@obrestad.org>
2017-06-01 17:27:15 +0200

    Revert "Authenticate for the relay"
    This reverts commit b95a45b2c2daeaec989ff09f006465b4aa7d9a0a.

--- manifests/baseconfig/mail.pp
+++ manifests/baseconfig/mail.pp
@@ -4,25 +4,17 @@ class profile::baseconfig::mail {
   $relay = hiera('profile::mail::relayhost')
   $rootmail = hiera('profile::mail::recipient')
 
-  $mail_user = hiera('profile::mail::user')
-  $mail_pass = hiera('profile::mail::pass')
-
   include ::augeas
 
   class { '::postfix':
     inet_interfaces     => '127.0.0.1',
     mynetworks          => '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128',
-    relayhost           => "${relay}:587",
+    relayhost           => $relay,
     root_mail_recipient => $rootmail,
     satellite           => true,
     smtp_listen         => '127.0.0.1',
   }
   postfix::config {
     'smtp_tls_security_level':  value  => 'may';
-  }
-
-  postfix::hash { '/etc/postfix/sasl_passwd':
-    ensure  => 'present',
-    content => "[${relay}]:587 ${mail_user}:${mail_pass}",
   }
 }
