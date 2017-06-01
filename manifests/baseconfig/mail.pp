b5a13c1d94d4c298ee0ce05d5e346cf5be4e47c3
Eigil Obrestad <eigil-git@obrestad.org>
2017-06-01 16:31:42 +0200

    Initial config for postfix mail relay

---
+++ manifests/baseconfig/mail.pp
@@ -0,0 +1,17 @@
+# Install postfix in sattelite mode, and configures it to relay mails trough a
+# mailserver.
+class profile::baseconfig::mail {
+  $relay = hiera('profile::mail::relayhost')
+  $rootmail = hiera('profile::mail::recipient')
+
+  class { '::postfix':
+    relay               => $relay,
+    inet_interfaces     => '127.0.0.1',
+    mydestination       => '',
+    mynetworks          => '127.0.0.0/8, [::1]/128',
+    relayhost           => $relay,
+    root_mail_recipient => $rootmail,
+    satellite           => True,
+    smtp_listen         => '127.0.0.1',
+  }
+}
