f6ef43f497b1a0294d9f956a076b09361092a6c7
Eigil Obrestad <eigil-git@obrestad.org>
2017-06-01 16:43:21 +0200

    Include augeas

--- manifests/baseconfig/mail.pp
+++ manifests/baseconfig/mail.pp
@@ -4,6 +4,8 @@ class profile::baseconfig::mail {
   $relay = hiera('profile::mail::relayhost')
   $rootmail = hiera('profile::mail::recipient')
 
+  include ::augeas
+
   class { '::postfix':
     inet_interfaces     => '127.0.0.1',
     mynetworks          => '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128',
