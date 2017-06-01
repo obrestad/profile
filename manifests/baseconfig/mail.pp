be2b89a1ae9045e6dab85f7f59717308b39fa5b4
Eigil Obrestad <eigil-git@obrestad.org>
2017-06-01 16:39:19 +0200

    Augeas fix

--- manifests/baseconfig/mail.pp
+++ manifests/baseconfig/mail.pp
@@ -6,7 +6,7 @@ class profile::baseconfig::mail {
 
   class { '::postfix':
     inet_interfaces     => '127.0.0.1',
-    mynetworks          => '127.0.0.0/8, [::1]/128',
+    mynetworks          => '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128',
     relayhost           => $relay,
     root_mail_recipient => $rootmail,
     satellite           => true,
