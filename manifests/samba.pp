3b75a2366ce001beff6ccc922ff91f7b5fd5f1e8
Eigil Obrestad <eigil-git@obrestad.org>
2018-04-28 22:27:01 +0200

    Get all data from hiera

--- manifests/samba.pp
+++ manifests/samba.pp
@@ -1,5 +1,7 @@
+# Installs a samba-server and sets up users and shares.
 class profile::samba {
-  $samba_eigil_pass = hiera('profile::user::eigil::samba::pass')
+  $samba_users = hiera_hash('profile::user::samba')
+  $shares = hiera_hash('profile::samba::shares')
 
   class {'samba::server':
     workgroup     => 'rothaugane',
@@ -8,40 +10,23 @@ class profile::samba {
     security      => 'user'
   }
 
-  samba::server::user { 'eigil':
-    password => $samba_eigil_pass,
+  $samba_users.each | $username, $password | {
+    samba::server::user { $username:
+      password => $password,
+    }
   }
 
-  samba::server::share {'archive':
-    comment              => 'Archived stuff',
-    path                 => '/srv/archive',
-    guest_only           => false,
-    guest_ok             => false,
-    guest_account        => 'nobody',
-    browsable            => true,
-    force_group          => 'users',
-  }
-
-  samba::server::share {'media':
-    comment              => 'Various media',
-    path                 => '/srv/media',
-    guest_only           => false,
-    guest_ok             => false,
-    guest_account        => 'nobody',
-    browsable            => true,
-    force_group          => 'users',
-    read_only            => false,
-  }
-
-  samba::server::share {'software':
-    comment              => 'Various software',
-    path                 => '/srv/software',
-    guest_only           => false,
-    guest_ok             => false,
-    guest_account        => 'nobody',
-    browsable            => true,
-    force_group          => 'users',
-    read_only            => false,
+  $shares.each | $sharename, $data | {
+    samba::server::share {'archive':
+      comment       => $data['comment'],
+      path          => $data['path'],
+      guest_only    => false,
+      guest_ok      => $data['guest_ok'],
+      guest_account => 'nobody',
+      browsable     => $data['browsable'],
+      force_group   => 'users',
+      read_onlu     => $data['read_only'],
+    }
   }
 
   firewall { '011 accept incoming SAMBA TCP':
