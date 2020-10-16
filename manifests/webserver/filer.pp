35c81a3ed85e0c203652f86a0d529daa35d00191
Eigil Obrestad <eigil-git@obrestad.org>
2020-10-16 21:45:42 +0200

    Create web-folder

--- manifests/webserver/filer.pp
+++ manifests/webserver/filer.pp
@@ -16,20 +16,11 @@ class profile::webserver::filer {
     ],
   }
 
-  #apache::vhost { "${::fqdn} https":
-  #  servername    => $::fqdn,
-  #  port          => '443',
-  #  docroot       => "/var/www/${::fqdn}",
-  #  ssl           => true,
-  #  ssl_cert      => "/etc/letsencrypt/live/${::fqdn}/fullchain.pem",
-  #  ssl_key       => "/etc/letsencrypt/live/${::fqdn}/privkey.pem",
-  #}
-
-  #letsencrypt::certonly { $::fqdn:
-  #  domains       => [$::fqdn],
-  #  plugin        => 'webroot',
-  #  webroot_paths => ["/var/www/${::fqdn}"],
-  #  require       => Apache::Vhost["${::fqdn} http"],
-  #  manage_cron   => true,
-  #}
+  file { '/home/eigil/www':
+    ensure   => 'directory',
+    owner    => 'eigil',
+    group    => 'www-data',
+    mode     => '2755',
+    require  => User['eigil'],
+  }
 }
