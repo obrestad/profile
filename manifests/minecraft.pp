ad910ffa56272426ec460f30ea5c7fec0bf064b4
Eigil Obrestad <eigil-git@obrestad.org>
2020-11-03 20:39:06 +0100

    Pick up ports from hiera

--- manifests/minecraft.pp
+++ manifests/minecraft.pp
@@ -1,12 +1,17 @@
 # Istalls JAVA and configures the firewall to allow a minecraft-server.
 class profile::minecraft {
+  $ports = lookup('profile::minecraft::ports', {
+    'value_type'    => Array[Integer],
+    'default_value' => [ 25565 ],
+  })
+
   package { 'openjdk-8-jdk':
     ensure => 'present',
   }
 
   firewall { '50 Accept minecraft':
     proto  => 'tcp',
-    dport  => 25565,
+    dport  => $ports,
     action => 'accept',
   }
 }
