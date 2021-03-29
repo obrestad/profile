0cf04ba1a78d33b130a7f47d1b75bc88b8f8cb2a
Eigil Obrestad <eigil-git@obrestad.org>
2021-03-29 20:45:25 +0200

    Add factorio user

--- manifests/factorio.pp
+++ manifests/factorio.pp
@@ -1,5 +1,7 @@
 # Configures misc. for a factorio server
 class profile::factorio {
+  include ::profile::users::groups
+
   $ports = lookup('profile::factorio::ports', {
     'value_type'    => Array[Integer],
     'default_value' => [ 34197 ],
@@ -9,5 +11,16 @@ class profile::factorio {
     proto  => 'udp',
     dport  => $ports,
     action => 'accept',
+  }
+
+  user { 'factorio':
+    ensure         => present,
+    gid            => 'service',
+    home           => '/opt/factorio',
+    managehome     => true,
+    password       => '*',
+    purge_ssh_keys => true,
+    shell          => '/bin/bash',
+    uid            => '740',
   }
 }
