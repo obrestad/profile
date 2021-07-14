b02e2995ded586872d7d56a9d6804b5d30189715
Eigil Obrestad <eigil-git@obrestad.org>
2021-07-14 22:04:47 +0200

    Configure backup of general files

--- manifests/backup/general.pp
+++ manifests/backup/general.pp
@@ -1,50 +1,24 @@
-# Installs my backupscript, and configurs backup of configurations and home
-# folders.
+# Configures backup of /etc and home-folders
 class profile::backup::general {
-  $usr = hiera('profile::backup::user')
-  $hst = hiera('profile::backup::host')
-  $base_path = hiera('profile::backup::base_path')
+  $usr = lookup('profile::backup::user', String)
+  $base_path = lookup('profile::backup::base_path', String)
 
-  $pth = "${base_path}/general/${::fqdn}"
+  $pth = "${base_path}/homes/${::fqdn}"
   $folders = '/root /home /etc'
 
-  file { '/var/lib/backup-lib.sh':
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/profile/scripts/backup-lib.sh',
-  }
-  file { '/usr/local/sbin/remote-backup':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    source  => 'puppet:///modules/profile/scripts/remote-backup.sh',
-    require => File['/var/lib/backup-lib.sh'],
-  }
-  file { '/usr/local/sbin/clean-backup':
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/profile/scripts/clean-backup.py',
-  }
-  cron { 'general-backup':
-    command => "/usr/local/sbin/remote-backup ${usr} ${hst} ${pth} ${folders}",
-    user    => root,
+  @@cron { "general-backup-${::fqdn}":
+    command => "/usr/local/sbin/backup-folders ${usr} ${::fqdn} ${pth} ${folders}",
+    user    => $usr,
     hour    => [3, 9, 15, 21],
-    minute  => [0],
-    require => File['/usr/local/sbin/remote-backup'],
+    minute  => [10],
+    tag     => 'backup-pulls',
   }
 
-  @@cron{ "clean-general-backup${::fqdn}":
+  @@cron{ "clean-general-backup-${::fqdn}":
     command => "/usr/local/sbin/clean-backup ${pth} --silent --delete",
-    user    => root,
+    user    => $usr,
     hour    => [4],
-    minute  => [56],
-    require => File['/usr/local/sbin/clean-backup'],
+    minute  => [37],
     tag     => 'clean-backups',
-  }
-
-  file { '/usr/local/sbin/general-backup':
-    ensure => absent,
   }
 }
