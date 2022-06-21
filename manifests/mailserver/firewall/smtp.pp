d6ddf6736c76127935889d3f1f97a92fa53ec29c
Eigil Obrestad <eigil-git@obrestad.org>
2022-06-21 10:33:49 +0200

    Close the SMTPS port as we do not use it anymore.

--- manifests/mailserver/firewall/smtp.pp
+++ manifests/mailserver/firewall/smtp.pp
@@ -2,12 +2,12 @@
 class profile::mailserver::firewall::smtp {
   firewall { '010 accept incoming SMTP':
     proto  => 'tcp',
-    dport  => [25, 465, 587],
+    dport  => [25, 587],
     action => 'accept',
   }
   firewall { '010 v6 accept incoming SMTP':
     proto    => 'tcp',
-    dport    => [25, 465, 587],
+    dport    => [25, 587],
     action   => 'accept',
     provider => 'ip6tables',
   }
