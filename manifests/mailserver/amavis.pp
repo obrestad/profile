d47cb17a95207c25341a72a5a34f456b54962f32
Eigil Obrestad <eigil-git@obrestad.org>
2022-11-19 23:07:15 +0100

    More amavis-config

--- manifests/mailserver/amavis.pp
+++ manifests/mailserver/amavis.pp
@@ -3,9 +3,23 @@ class profile::mailserver::amavis {
   include ::profile::mailserver::amavis::install
   include ::profile::mailserver::amavis::service
 
+  $postmaster = lookup('profile::mail::postmaster', String)
+  $domains = lookup('profile::mail::domains', {
+    'default_value' => [],
+    'value_type'    => Array[String],
+  })
+
   profile::mailserver::amavis::config { 'general':
     data => {
       'mydomain' => $facts['networking']['domain'],
+      'myhostname' => $facts['networking']['fqdn'],
+      'mailfrom_notify_admin' => $postmaster,
+      'mailfrom_notify_recip' => $postmaster,
+      'mailfrom_notify_spamadmin' => $postmaster,
+      'recipient_delimiter' => '-'
+      'notify_method' => 'smtp:[127.0.0.1]:10025',
+      'forward_method' => 'smtp:[127.0.0.1]:10025',
+      'local_domains_acl' => [ '.$mydomain', $domains ],
     }
   }
 
@@ -17,6 +31,8 @@ class profile::mailserver::amavis {
 
   profile::mailserver::amavis::config { 'spamfilter':
     data => {
+      'sa_tag_level_deflt' => -999,
+      'spam_quarantine_to' => undef,
       'final_spam_destiny' => 'D_DISCARD',
       'bypass_spam_checks_maps' => [
         '\%bypass_spam_checks',
