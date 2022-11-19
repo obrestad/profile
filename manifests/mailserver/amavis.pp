62f7ecff9f49559d607c16faaa8a1c5a2a5b8c58
Eigil Obrestad <eigil-git@obrestad.org>
2022-11-19 22:34:59 +0100

    Use an array in amavis-config

--- manifests/mailserver/amavis.pp
+++ manifests/mailserver/amavis.pp
@@ -6,6 +6,11 @@ class profile::mailserver::amavis {
   profile::mailserver::amavis::config { 'spamfilter':
     data => {
       'final_spam_destiny' => 'D_DISCARD',
+      'bypass_spam_checks_maps' => [
+        '\%bypass_spam_checks',
+        '\@bypass_spam_checks_acl',
+        '\$bypass_spam_checks_re',
+      ],
     }
   }
 }
