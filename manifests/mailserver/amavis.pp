51369543927cd1e9cab8ad3c512e1fc058ad3737
Eigil Obrestad <eigil-git@obrestad.org>
2022-11-19 22:50:15 +0100

    More amavis-config

--- manifests/mailserver/amavis.pp
+++ manifests/mailserver/amavis.pp
@@ -3,6 +3,18 @@ class profile::mailserver::amavis {
   include ::profile::mailserver::amavis::install
   include ::profile::mailserver::amavis::service
 
+  profile::mailserver::amavis::config { 'general':
+    data => {
+      'mydomain' => $facts['networking']['domain'],
+    }
+  }
+
+  profile::mailserver::amavis::config { 'dkim':
+    data => {
+      'enable_dkim_verification' => 1,
+    }
+  }
+
   profile::mailserver::amavis::config { 'spamfilter':
     data => {
       'final_spam_destiny' => 'D_DISCARD',
