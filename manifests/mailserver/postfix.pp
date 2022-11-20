11872a3bb1c6748fb3db321ddd4e409a16e46a3b
Eigil Obrestad <eigil-git@obrestad.org>
2022-11-20 20:17:22 +0100

    Add amavis services to postfix

--- manifests/mailserver/postfix.pp
+++ manifests/mailserver/postfix.pp
@@ -13,27 +13,53 @@ class profile::mailserver::postfix {
 
   class { '::postfix':
     master_smtp       => 'smtp inet n - n - - smtpd
-  -o smtpd_recipient_restrictions=reject_invalid_helo_hostname,reject_non_fqdn_sender,reject_unknown_sender_domain,reject_non_fqdn_recipient,reject_unknown_recipient_domain,reject_unauth_destination,reject_rbl_client,zen.spamhaus.org,permit
-  -o smtpd_helo_required=yes
-  -o disable_vrfy_command=yes
-  -o content_filter=spamassassin',
+      -o smtpd_recipient_restrictions=reject_invalid_helo_hostname,reject_non_fqdn_sender,reject_unknown_sender_domain,reject_non_fqdn_recipient,reject_unknown_recipient_domain,reject_unauth_destination,reject_rbl_client,zen.spamhaus.org,permit
+      -o smtpd_helo_required=yes
+      -o disable_vrfy_command=yes
+      -o content_filter=spamassassin',
     master_submission => 'submission inet n - n - - smtpd
-  -o syslog_name=postfix/submission
-  -o smtpd_tls_security_level=encrypt
-  -o smtpd_sasl_auth_enable=yes
-  -o smtpd_tls_auth_only=yes
-  -o smtpd_reject_unlisted_recipient=no
-  -o smtpd_client_restrictions=permit_sasl_authenticated,permit_mynetworks,reject
-  -o smtpd_helo_restrictions=
-  -o smtpd_sender_restrictions=
-  -o smtpd_recipient_restrictions=
-  -o smtpd_relay_restrictions=permit_sasl_authenticated,reject_unauth_destination,reject
-  -o milter_macro_daemon_name=ORIGINATING
-  -o disable_vrfy_command=yes
-  -o content_filter=spamassassin',
+      -o syslog_name=postfix/submission
+      -o smtpd_tls_security_level=encrypt
+      -o smtpd_sasl_auth_enable=yes
+      -o smtpd_tls_auth_only=yes
+      -o smtpd_reject_unlisted_recipient=no
+      -o smtpd_client_restrictions=permit_sasl_authenticated,permit_mynetworks,reject
+      -o smtpd_helo_restrictions=
+      -o smtpd_sender_restrictions=
+      -o smtpd_recipient_restrictions=
+      -o smtpd_relay_restrictions=permit_sasl_authenticated,reject_unauth_destination,reject
+      -o milter_macro_daemon_name=ORIGINATING
+      -o disable_vrfy_command=yes
+      -o content_filter=spamassassin',
     master_entries => [
   'spamassassin unix - n n - - pipe
-  user=debian-spamd argv=/usr/bin/spamc -f -e /usr/sbin/sendmail -oi -f ${sender} ${recipient}',
+      user=debian-spamd argv=/usr/bin/spamc -f -e /usr/sbin/sendmail -oi -f ${sender} ${recipient}',
+  'smtp-amavis        unix    -    -    n    -    2    smtp
+      -o smtp_data_done_timeout=1200
+      -o smtp_send_xforward_command=yes
+      -o disable_dns_lookups=yes
+      -o max_use=20',
+  '127.0.0.1:10025    inet    n    -    n    -    -    smtpd
+      -o content_filter=
+      -o local_recipient_maps=
+      -o relay_recipient_maps=
+      -o smtpd_restriction_classes=
+      -o smtpd_delay_reject=no
+      -o smtpd_client_restrictions=permit_mynetworks,reject
+      -o smtpd_helo_restrictions=
+      -o smtpd_sender_restrictions=
+      -o smtpd_recipient_restrictions=permit_mynetworks,reject
+      -o smtpd_data_restrictions=reject_unauth_pipelining
+      -o smtpd_end_of_data_restrictions=
+      -o mynetworks=127.0.0.0/8
+      -o smtpd_error_sleep_time=0
+      -o smtpd_soft_error_limit=1001
+      -o smtpd_hard_error_limit=1000
+      -o smtpd_client_connection_count_limit=0
+      -o smtpd_client_connection_rate_limit=0
+      -o receive_override_options=no_header_body_checks,no_unknown_recipient_checks,no_milters
+      -o local_header_rewrite_clients=
+      -o smtpd_milters=',
     ],
   }
 
