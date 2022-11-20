5f4cc39d1f02c4da00ea5eabfb2e2ee17f040f1f
Eigil Obrestad <eigil-git@obrestad.org>
2022-11-20 20:13:12 +0100

    Linting

--- manifests/mailserver/amavis.pp
+++ manifests/mailserver/amavis.pp
@@ -11,29 +11,30 @@ class profile::mailserver::amavis {
 
   profile::mailserver::amavis::config { 'general':
     data => {
-      'mydomain' => $facts['networking']['domain'],
-      'myhostname' => $facts['networking']['fqdn'],
-      'mailfrom_notify_admin' => $postmaster,
-      'mailfrom_notify_recip' => $postmaster,
+      'mydomain'                  => $facts['networking']['domain'],
+      'myhostname'                => $facts['networking']['fqdn'],
+      'mailfrom_notify_admin'     => $postmaster,
+      'mailfrom_notify_recip'     => $postmaster,
       'mailfrom_notify_spamadmin' => $postmaster,
-      'recipient_delimiter' => '-',
-      'notify_method' => 'smtp:[127.0.0.1]:10025',
-      'forward_method' => 'smtp:[127.0.0.1]:10025',
-      'local_domains_acl' => [ '.$mydomain' ] + $domains,
+      'recipient_delimiter'       => '-',
+      'notify_method'             => 'smtp:[127.0.0.1]:10025',
+      'forward_method'            => 'smtp:[127.0.0.1]:10025',
+      'local_domains_acl'         => [ '.$mydomain' ] + $domains,
     }
   }
 
   profile::mailserver::amavis::config { 'dkim':
     data => {
       'enable_dkim_verification' => 1,
+      'enable_dkim_signing'      => 1,
     }
   }
 
   profile::mailserver::amavis::config { 'spamfilter':
     data => {
-      'sa_tag_level_deflt' => -999,
-      'spam_quarantine_to' => undef,
-      'final_spam_destiny' => 'D_DISCARD',
+      'sa_tag_level_deflt'      => -999,
+      'spam_quarantine_to'      => undef,
+      'final_spam_destiny'      => 'D_DISCARD',
       'bypass_spam_checks_maps' => [
         '\%bypass_spam_checks',
         '\@bypass_spam_checks_acl',
