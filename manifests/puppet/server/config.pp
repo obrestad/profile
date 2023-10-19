a09474f2a5a4cca0d4137ab7e0512d05868951f9
Eigil Obrestad <eigil-git@obrestad.org>
2023-10-19 12:47:17 +0200

    Configure a report-URL

--- manifests/puppet/server/config.pp
+++ manifests/puppet/server/config.pp
@@ -1,5 +1,9 @@
 # Configures the puppetmaster
 class profile::puppet::server::config {
+  $report_url = lookup('profile::puppet::report::url', {
+    'default_value' => false,
+    'value_type'    => Variant[String, Boolean],
+  })
   $puppetca = lookup('profile::puppet::caserver', Stdlib::Fqdn)
 
   $puppetdb = lookup('profile::puppet::db::server', {
@@ -54,4 +58,37 @@ class profile::puppet::server::config {
     notify  => Service['puppetserver'],
     require => Package['puppetserver'],
   }
+
+  if($report_url) {
+    ini_setting { 'Puppet Report type':
+      ensure  => present,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'master',
+      setting => 'reports',
+      value   => 'http',
+    }
+
+    ini_setting { 'Puppet Report url':
+      ensure  => present,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'master',
+      setting => 'reports',
+      value   => $report_url,
+    }
+  } else {
+    ini_setting { 'Puppet Report type':
+      ensure  => absent,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'master',
+      setting => 'reports',
+    }
+
+    ini_setting { 'Puppet Report url':
+      ensure  => absent,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'master',
+      setting => 'reports',
+    }
+  }
+
 }
