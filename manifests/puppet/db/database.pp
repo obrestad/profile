f880d1b2fe5e9271b56e702007d8153b9c759026
Eigil Obrestad <eigil-git@obrestad.org>
2024-01-14 14:07:42 +0100

    Blast from the past removed

--- manifests/puppet/db/database.pp
+++ manifests/puppet/db/database.pp
@@ -1,14 +1,14 @@
 # Configures a database for puppetdb
 class profile::puppet::db::database {
-  $dbname = hiera('profile::puppet::db::database::name', {
+  $dbname = lookup('profile::puppet::db::database::name', {
     'default_value' => 'puppetdb',
     'value_type'    => String,
   })
-  $dbuser = hiera('profile::puppet::db::database::user', {
+  $dbuser = lookup('profile::puppet::db::database::user', {
     'default_value' => 'puppetdb',
     'value_type'    => String,
   })
-  $dbpass = hiera('profile::puppet::db::database::pass')
+  $dbpass = lookup('profile::puppet::db::database::pass')
 
   ::postgresql::server::extension { 'pg_trgm':
     database  => $dbname,
