13fae882eee4615c76fcbd84e8a03d091b30073a
Eigil Obrestad <eigil-git@obrestad.org>
2024-05-13 19:56:08 +0200

    Is an = an ? or not ?

--- manifests/baseconfig/networking.pp
+++ manifests/baseconfig/networking.pp
@@ -20,8 +20,8 @@ class profile::baseconfig::networking {
   })
 
   $networks.each | $netname, $data | {
-    $ipv4 = ('ipv4' in $data) = { true => $data['ipv4']['addresses'], false => []}
-    $ipv6 = ('ipv6' in $data) = { true => $data['ipv6']['addresses'], false => []}
+    $ipv4 = ('ipv4' in $data) ? { true => $data['ipv4']['addresses'], false => []}
+    $ipv6 = ('ipv6' in $data) ? { true => $data['ipv6']['addresses'], false => []}
 
     ::profile::baseconfig::networking::interface { $netname:
       addresses         => $ipv4 + $ipv6 - [ undef ],
@@ -36,8 +36,8 @@ class profile::baseconfig::networking {
   }
 
   $vlans.each | $netname, $data | {
-    $ipv4 = ('ipv4' in $data) = { true => $data['ipv4']['addresses'], false => []}
-    $ipv6 = ('ipv6' in $data) = { true => $data['ipv6']['addresses'], false => []}
+    $ipv4 = ('ipv4' in $data) ? { true => $data['ipv4']['addresses'], false => []}
+    $ipv6 = ('ipv6' in $data) ? { true => $data['ipv6']['addresses'], false => []}
 
     ::profile::baseconfig::networking::interface { $netname:
       addresses         => $ipv4 + $ipv6 - [ undef ],
