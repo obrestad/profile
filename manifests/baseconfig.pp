11aee79e06dca03727525facc3030cb14b81ec66
Eigil Obrestad <eigil-git@obrestad.org>
2024-08-09 20:58:36 +0200

    Make network-config optional

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -1,9 +1,17 @@
 # Base-configuration of the hosts.
 class profile::baseconfig {
+  $networking = lookup('profile::baseconfig::network::config', {
+    'default_value' => true,
+    'value_type'    => Boolean,
+  })
+
+  if($networking) {
+    include ::profile::baseconfig::networking
+  }
+
   include ::profile::backup::general
 
   include ::profile::baseconfig::disk
-  include ::profile::baseconfig::networking
   include ::profile::baseconfig::ntp
   include ::profile::baseconfig::ssh
   include ::profile::baseconfig::software
