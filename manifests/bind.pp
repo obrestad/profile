8cb55dc7e5403760b2d83125bc086d685b660d9c
Eigil Obrestad <eigil-git@obrestad.org>
2025-03-14 21:40:41 +0100

    Seed root

--- manifests/bind.pp
+++ manifests/bind.pp
@@ -3,4 +3,9 @@ class profile::bind {
   include bind
   bind::server::conf { '/etc/bind/named.conf':
   }
+
+  file { '/etc/bind/db.root':
+    ensure => link, 
+    target => '/usr/share/dns/root.hints',
+  }
 }
